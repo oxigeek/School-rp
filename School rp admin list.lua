cc8f5edc38af4a14b73d47876e608440c3e6d418
OV3RDRIVEDev <134893635+OV3RDRIVEDev@users.noreply.github.com>
2023-06-10 21:06:13 +0200

    Update School rp admin list.lua

--- School rp admin list.lua
+++ School rp admin list.lua
@@ -1,13 +1,21 @@
 return {
 	{
-		["Name"] = "Ultra Admin+",
+		["Name"] = "Fondateur",
 		["RankPoint"] = 35,
 		["players"] = {
 			--Equipe de build
-			"oxigeek_youtube",  --Oxi_Fox
-			"0V3RDRIVE_Dev",    --0V3RDRIVE_Dev
-                        "zhaviaa212",       --Zhavia
-                        "Zhavia_secour",    --Zhavia
+                        "zhaviaa212",         --Zhavia
+                        "Zhavia_secour",      --Zhavia
+			"oxigeek_youtube",    --Oxi_Fox
+		}
+	},
+
+	{
+		["Name"] = "Gérant Staff",
+		["RankPoint"] = 35,
+		["players"] = {
+			--Equipe de build
+			"0V3RDRIVE_Dev",      --0V3RDRIVE_Dev
 		}
 	},
 
@@ -16,11 +24,9 @@ return {
 		["RankPoint"] = 30,
 		["players"] = {
 			--Equipe de build
-			--"oxigeek_youtube", 
-			"Fegurie45", --Coach
-			--"GUNHS586", --Taki
-			"PetitBiscuit7890", --???
-			"Netey_TheKing", --???
+			"Fegurie45",          --Coach
+			"PetitBiscuit7890",   --???
+			"Netey_TheKing",      --???
 		}
 	},
 
@@ -28,7 +34,7 @@ return {
 		["Name"] = "Super admin",
 		["RankPoint"] = 25,
 		["players"] = {
-			"mathis83game", --Alcatraz
+			"mathis83game",       --Alcatraz
 			--"Player", --
 			--"Player", --
 			--"Player", --
@@ -43,36 +49,36 @@ return {
 		["Name"] = "Admin",
 		["RankPoint"] = 20,
 		["players"] = {
-			--"FrEmojis",       --Night
-			"NewFunRP",         --ExStar
-			"DelonAlain",       --SLAYK
-			"HyerToujoursBan",  --Hyer
-			"maxime26422",      --poolu
-			"Jxsttt_Ec0oo",     --Eco
-			"noob_masteurs",    --sardine
-			"leroilion97",      --La_Fourmi
-			--"Player",     --
-			--"mi0gi",          --mi0gi
-			--"Oasis_better",   --
-			"Enetritz",         --Enetritz
-			"BRocoliiYYTTB",    --chacha 
-			--"KUNY_ROBLOX",    --Joestar
-			"Shaid66x",         --Couscous
-			"zorjfb",         --Ethan
-			--"serena_glb",     --Inori
-			"Breenstarr",       --Alvaro
-			--"Artiquiche",     --CST
+			--"FrEmojis",        --Night
+			"NewFunRP",          --ExStar
+			"DelonAlain",        --SLAYK
+			"HyerToujoursBan",   --Hyer
+			"maxime26422",       --poolu
+			"Jxsttt_Ec0oo",      --Eco
+			"noob_masteurs",     --sardine
+			"leroilion97",       --La_Fourmi
+			--"Player",          --
+			--"mi0gi",           --mi0gi
+			--"Oasis_better",    --
+			"Enetritz",          --Enetritz
+			"BRocoliiYYTTB",     --chacha 
+			--"KUNY_ROBLOX",     --Joestar
+			"Shaid66x",          --Couscous
+			"zorjfb",            --Ethan
+			--"serena_glb",      --Inori
+			"Breenstarr",        --Alvaro
+			--"Artiquiche",      --CST
 			"WSA_amine55",       --Snow
 			"Loupbleu_VoiceChat",--loubleu 
-			"LilHokai", --Dembele
-			"OverWater495", --Jhon
-			"Bad_meuf26", --Pêche
-			"Philippeveil",  --SCP-096
-			"Fr0zen_Gamer63", --Panthera
-			"ravenfield91", --babaduck 
-			"Lunalabosse1234", --lunaptdrrr
-			"MAEKAN3000", --Lord_Radde
-			"Azxcomiks13", --Comiks
+			"LilHokai",          --Dembele
+			"OverWater495",      --Jhon
+			"Bad_meuf26", 	     --Pêche
+			"Philippeveil",      --SCP-096
+			"Fr0zen_Gamer63",    --Panthera
+			"ravenfield91",      --babaduck 
+			"Lunalabosse1234",   --lunaptdrrr
+			"MAEKAN3000",        --Lord_Radde
+			"Azxcomiks13",       --Comiks
 		}
 	},
 }
