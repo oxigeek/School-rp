ba0155f698d6a16f3fd0a3cc631d0c0a6984965a
oxigeek <80268381+oxigeek@users.noreply.github.com>
2023-06-10 11:49:02 +0200

    Create School rp admin list.lua

---
+++ School rp admin list.lua
@@ -0,0 +1,63 @@
+{
+	{
+		["Name"] = "Mega admin",
+		["RankPoint"] = 30,
+		["players"] = {
+			--Equipe de build
+			"oxigeek_youtube", 
+			"Fegurie45", 
+			"zhaviaa212", 
+			"Zhavia_secour", 
+			--"GUNHS586",
+			"PetitBiscuit7890",
+			"Netey_TheKing",
+			"0V3RDRIVE_Dev",    --Overdrive
+		}
+	},
+
+	{
+		["Name"] = "Super admin",
+		["RankPoint"] = 25,
+		["players"] = {
+
+		}
+	},
+
+
+	{
+		["Name"] = "Admin",
+		["RankPoint"] = 20,
+		["players"] = {
+			--"FrEmojis",       --Night
+			"NewFunRP",         --ExStar
+			"DelonAlain",       --SLAYK
+			"HyerToujoursBan",  --Hyer
+			"maxime26422",      --poolu
+			"Jxsttt_Ec0oo",     --Eco
+			"noob_masteurs",    --sardine
+			"leroilion97",      --La_Fourmi
+			"mathis83game",     --alcatraz  
+			--"mi0gi",          --mi0gi
+			--"Oasis_better",   --
+			"Enetritz",         --Enetritz
+			"BRocoliiYYTTB",    --chacha 
+			--"KUNY_ROBLOX",    --Joestar
+			"Shaid66x",         --Couscous
+			"zorjfb",         --Ethan
+			--"serena_glb",     --Inori
+			"Breenstarr",       --Alvaro
+			--"Artiquiche",     --CST
+			"WSA_amine55",       --Snow
+			"Loupbleu_VoiceChat",--loubleu 
+			"LilHokai",
+			"OverWater495",
+			"Bad_meuf26",
+			"Philippeveil",
+			"Fr0zen_Gamer63",
+			"ravenfield91",
+			"Lunalabosse1234",
+			"MAEKAN3000",
+			"Azxcomiks13",
+		}
+	},
+}
