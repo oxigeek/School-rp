3cb68527b70fdeae258e506254dca977162fa1c3
Oxi_fox <80268381+oxigeek@users.noreply.github.com>
2023-09-03 19:18:34 +0200

    Update School rp admin list.lua

--- School rp admin list.lua
+++ School rp admin list.lua
@@ -1,167 +1,173 @@
 return {
-	{
-		["Name"] = "Fondateur",
-		["RankPoint"] = 55 ,
-		["players"] = {
-			--Equipe de build
-			"oxigeek_youtube",    --Oxi_Fox
-			"0xiFox",             --Oxi_Fox alt
-		}
+	["Moderation"] = {
+		{
+			["Name"] = "Fondateur",
+			["RankPoint"] = 55 ,
+			["players"] = {
+				--Equipe de build
+				"oxigeek_youtube",    --Oxi_Fox
+				"0xiFox",             --Oxi_Fox alt
+			}
+		},
+
+		{
+			["Name"] = "Gérant Staff",
+			["RankPoint"] = 50,
+			["players"] = {
+				--Equipe de build
+				--"Players",         --
+				"Poulakisi",          --Mr.Poulaki
+				"Poulakiis",          --Mr.Poulaki
+				"Loupbleu_VoiceChat", --Oxi alt 
+			}
+		},
+
+		{
+			["Name"] = "Sous Gérant Staff",
+			["RankPoint"] = 40,
+			["players"] = {
+				--Equipe de build
+				--"Players",      --
+			}
+		},
+
+		{
+			["Name"] = "Super Admin",
+			["RankPoint"] = 35,
+			["players"] = {
+
+			}
+		},
+
+
+		{
+			["Name"] = "Admin",
+			["RankPoint"] = 30,
+			["players"] = {
+				--"AdvanS7evn",            --Advan
+				"ALEXENDRE_192",         --GATOR
+				"alexendre192",          --GATOR ALT
+			}
+		},
+
+
+		{
+			["Name"] = "Modérateur Senior",
+			["RankPoint"] = 20,
+			["players"] = {
+				--"RaionO_O",            --Vic
+				"Msne76",              --MAE
+				"youmielol",           --MAE alt
+			}
+		},
+
+		{
+			["Name"] = "Modérateur",
+			["RankPoint"] = 15,
+			["players"] = {
+				--"AdvanS7evn",            --Advan
+				--"Nako_28",               --Boubacar Nawel
+				--"AlloBassemOeCgreg",     --VINCENT CANICULE
+				"epeistenoir9",          --Landers
+				--"S4turne_lly",           --Landers alt
+				"MechantMeusieu",        --Fripouille
+				"MAEKAN3000",            --Busier
+				"MAEKAN3001",            --Biser 
+			}
+		},
+
+		{
+			["Name"] = "Modérateur en test",
+			["RankPoint"] = 12,
+			["players"] = {
+
+
+				--"Paradise_III",
+
+				--"Loupbleu_VoiceChat",  --Staff secours
+				--"Mellll_ek",           --Mell
+
+
+				--"Bad_meuf26",          --Peche
+				"Oshibanane",          --Oshi
+				"Adriax51100",         --Adriax
+				"Azxcomiks13",         --Comiks
+				--"encoreban54895",      -- Oshi
+				--"Azxcomiks13",
+
+
+			}
+		},
+
+		{
+			["Name"] = "Developpeur",
+			["RankPoint"] = 3,
+			["players"] = {
+
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+			},
+		},
+
+		{
+			["Name"] = "gérant wl",
+			["RankPoint"] = 3,
+			["players"] = {
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+			},
+		},
+
+		{
+			["Name"] = "VIP",
+			["RankPoint"] = 10,
+			["players"] = {
+				"Sud0craft",
+				"TouyaShy",
+				--"Mellll_ek",
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         -- 
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+				--"Players",         --
+			},
+		},
 	},
-
-	{
-		["Name"] = "Gérant Staff",
-		["RankPoint"] = 50,
-		["players"] = {
-			--Equipe de build
-			--"Players",         --
-			"Poulakisi",          --Mr.Poulaki
-			"Poulakiis",          --Mr.Poulaki
-			"Loupbleu_VoiceChat", --Oxi alt 
-		}
-	},
-
-	{
-		["Name"] = "Sous Gérant Staff",
-		["RankPoint"] = 40,
-		["players"] = {
-			--Equipe de build
-			--"Players",      --
-		}
-	},
-
-	{
-		["Name"] = "Super Admin",
-		["RankPoint"] = 35,
-		["players"] = {
-			
-		}
-	},
-
-
-	{
-		["Name"] = "Admin",
-		["RankPoint"] = 30,
-		["players"] = {
-			--"AdvanS7evn",            --Advan
-			"ALEXENDRE_192",         --GATOR
-			"alexendre192",          --GATOR ALT
-		}
-	},
-
 	
-	{
-		["Name"] = "Modérateur Senior",
-		["RankPoint"] = 20,
-		["players"] = {
-			--"RaionO_O",            --Vic
-			"Msne76",              --MAE
-			"youmielol",           --MAE alt
-		}
-	},
-
-	{
-		["Name"] = "Modérateur",
-		["RankPoint"] = 15,
-		["players"] = {
-			--"AdvanS7evn",            --Advan
-			--"Nako_28",               --Boubacar Nawel
-			--"AlloBassemOeCgreg",     --VINCENT CANICULE
-			"epeistenoir9",          --Landers
-			--"S4turne_lly",           --Landers alt
-			"MechantMeusieu",        --Fripouille
-			"MAEKAN3000",            --Busier
-			"MAEKAN3001",            --Biser 
-		}
-	},
-
-	{
-		["Name"] = "Modérateur en test",
-		["RankPoint"] = 12,
-		["players"] = {
-			
-			
-			--"Paradise_III",
-			
-			--"Loupbleu_VoiceChat",  --Staff secours
-			--"Mellll_ek",           --Mell
-
-			
-			--"Bad_meuf26",          --Peche
-			"Oshibanane",          --Oshi
-			"Adriax51100",         --Adriax
-			"Azxcomiks13",         --Comiks
-			--"encoreban54895",      -- Oshi
-			--"Azxcomiks13",
-			
-			
-		}
-	},
-
-	{
-		["Name"] = "Developpeur",
-		["RankPoint"] = 3,
-		["players"] = {
-			
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-		},
-	},
-
-	{
-		["Name"] = "gérant wl",
-		["RankPoint"] = 3,
-		["players"] = {
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-		},
-	},
-
-	{
-		["Name"] = "VIP",
-		["RankPoint"] = 10,
-		["players"] = {
-			"Sud0craft",
-			"TouyaShy",
-			--"Mellll_ek",
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         -- 
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-			--"Players",         --
-		},
-	},
+	
+	
+	
 }
