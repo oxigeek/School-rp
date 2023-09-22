214df30427bbe49f98d25dc96e777c528a5a2326
Oxi_fox <80268381+oxigeek@users.noreply.github.com>
2023-09-22 08:01:05 +0200

    Update School rp admin list.lua

--- School rp admin list.lua
+++ School rp admin list.lua
@@ -100,6 +100,7 @@ return {
 				"nicolasclaus",    --Nicolasclaus
 				"leGnome9",        --Gnome alt
 				"GnomeOrphee74",   --Gnome
+				"GnomeOrphee74",   --Gnome alt2
 				"Inspecteur_Li",   --Mme. Livi
 
 
